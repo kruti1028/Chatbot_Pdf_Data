e6cd94a511070cd9dd503b0d089e1590a0ef4fc6
kruti kikani <126723087+kruti1028@users.noreply.github.com>
2023-12-19 12:00:31 -0600

    Update visu.py

--- visu.py
+++ visu.py
@@ -1,96 +1,77 @@
-from flask import Flask, render_template
 import sqlite3
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-from IPython.display import HTML
+import plotly.express as px
 
-
-app = Flask(__name__)
-
-def generate_sunburst_chart_from_db(db_path):
-    # Generate a color palette
-    color_palette = plt.cm.tab20.colors  # Using Matplotlib's tab20 colormap
-    color_index = 0
-
-    # Function to get the next color from the palette
-    def get_next_color():
-        nonlocal color_index
-        color = color_palette[color_index % len(color_palette)]
-        color_index += 1
-        return f'rgb({color[0]*255}, {color[1]*255}, {color[2]*255})'
-
-    # Map to store colors for each type of insurance
-    type_color_map = {}
-
-    # Connect to the SQLite database
+def fetch_data_from_db(db_path, query):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-
-    # Query to join insurance_policies and categories tables
-    query = '''
-    SELECT p.id, p.total_coverage_amount, p.type_of_insurance, p.annual_premium, p.insurer, 
-           p.insured, p.issue_date, p.renewal_date, p.policy_number, 
-           c.category_name, c.covered, c.not_covered
-    FROM insurance_policies p
-    LEFT JOIN categories c ON p.id = c.policy_id
-    '''
     cursor.execute(query)
-    rows = cursor.fetchall()
-
-    # Close the database connection
+    data = cursor.fetchall()
     conn.close()
+    return data
 
-    # Organize the data for the sunburst chart
-    labels, parents, values, hovertext, colors = [], [], [], [], []
-    for row in rows:
-        # Extract data from each row
-        policy_id, total_coverage_amount, type_of_insurance, annual_premium, insurer, insured, issue_date, renewal_date, policy_number, category_name, covered, not_covered = row
+def generate_hovertext_for_root(data):
+    if not data:
+        return "No Data Available"
+    row = data[0]
+    total_coverage_amount, annual_premium, insurer, insured, issue_date, renewal_date, policy_number = row[1], row[3], row[4], row[5], row[6], row[7], row[8]
+    hovertext = f"Total Coverage Amount: {total_coverage_amount}<br>" \
+                f"Annual Premium: {annual_premium}<br>" \
+                f"Insurer: {insurer}<br>" \
+                f"Insured: {insured}<br>" \
+                f"Issue Date: {issue_date}<br>" \
+                f"Renewal Date: {renewal_date}<br>" \
+                f"Policy Number: {policy_number}"
+    return hovertext
 
-        # Add data to labels, parents, values, hovertext, and colors
-        # Root label for each policy
-        root_label = f"Policy {policy_id}: {total_coverage_amount}"
-        if root_label not in labels:
-            labels.append(root_label)
-            parents.append('')
-            values.append(100)  # Static value or calculated
-            hovertext.append(f"Insurer: {insurer}<br>Insured: {insured}<br>Total Coverage Amount: {total_coverage_amount}<br>Annual Premium: {annual_premium}<br>Renewal Date: {renewal_date}<br>Issue Date: {issue_date}")
-            colors.append("lightgray")  # Default color for root
+def generate_sunburst_chart_from_db(data):
+    # Define custom colors for insurance types and other elements
+    custom_colors = {
+        "Total Coverage": "#FFFFFF",
+        "Life Insurance": "#1f77b4",  # Blue
+        "Health Insurance": "#ff7f0e",  # Orange
+        "Auto Insurance": "#2ca02c",  # Green
+    }
 
-        # Insurance type
-        if type_of_insurance and type_of_insurance not in labels:
-            labels.append(type_of_insurance)
-            parents.append(root_label)
-            values.append(20)  # Static value or calculated
-            hovertext.append(f"Type of Insurance: {type_of_insurance}")
-            # Assign a color from the palette
-            if type_of_insurance not in type_color_map:
-                type_color_map[type_of_insurance] = get_next_color()
-            colors.append(type_color_map[type_of_insurance])
+    total_coverage_amount = data[0][1] if data else "Not specified"
+    root_hovertext = generate_hovertext_for_root(data)
+    
+    labels, parents, values, hovertext, colors = ['Total Coverage: ' + total_coverage_amount], [''], [0], [root_hovertext], [custom_colors.get("Total Coverage", "lightgray")]
 
-        # Categories
-        if category_name and category_name not in labels:
-            labels.append(category_name)
-            parents.append(type_of_insurance)
-            values.append(10)  # Static value or calculated
-            hovertext.append(f"Category: {category_name}")
-            colors.append("lightblue")  # Color for categories
+    for row in data:
+        _, _, type_of_insurance, _, _, _, _, _, _, category_name, covered, not_covered = row
 
-        # Covered and Not Covered Items
-        for item in covered.split(','):
-            labels.append(f"Covered: {item}")
-            parents.append(category_name)
-            values.append(5)  # Static value or calculated
-            hovertext.append(f"Covered Item: {item}")
-            colors.append("lightgreen")  # Color for covered items
+        type_label = type_of_insurance + " Insurance"
+        if type_label not in labels:
+            labels.append(type_label)
+            parents.append('Total Coverage: ' + total_coverage_amount)
+            values.append(0)
+            hovertext.append(type_label)
+            colors.append(custom_colors.get(type_label, "#d62728"))  # Default color if not specified
 
-        for item in not_covered.split(','):
-            labels.append(f"Not Covered: {item}")
-            parents.append(category_name)
-            values.append(5)  # Static value or calculated
-            hovertext.append(f"Not Covered Item: {item}")
-            colors.append("pink")  # Color for not covered items
+        category_label = f"{type_label}: {category_name}"
+        if category_label not in labels:
+            labels.append(category_label)
+            parents.append(type_label)
+            values.append(0)
+            hovertext.append(category_label)
+            colors.append(custom_colors.get("Category", "lightblue"))  # Default category color
 
-    # Create and configure the sunburst chart
+        for item in (covered + ',' + not_covered).split(','):
+            if item.strip():
+                item_label = f"{category_label}: {'Covered' if item in covered else 'Not Covered'} - {item.strip()}"
+                labels.append(item_label)
+                parents.append(category_label)
+                values.append(1)
+                hovertext.append(item_label)
+                colors.append(custom_colors.get("Covered" if 'Covered' in item_label else "Not Covered", "lightgreen" if 'Covered' in item_label else "pink"))
+
+    # Update values for types and categories
+    for i in range(len(labels)-1, 0, -1):  # Reverse order to accumulate values
+        parent_index = labels.index(parents[i])
+        values[parent_index] += values[i]
+
+    # Create the sunburst chart
     fig = go.Figure(go.Sunburst(
         labels=labels,
         parents=parents,
@@ -98,21 +79,28 @@ def generate_sunburst_chart_from_db(db_path):
         hoverinfo="text",
         hovertext=hovertext,
         branchvalues="total",
-        marker=dict(colors=colors)  # Apply the colors
+        marker=dict(colors=colors),
+        insidetextorientation='radial'
     ))
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
-    # Convert chart to HTML
-    chart_html = fig.to_html(full_html=False, include_plotlyjs='cdn', default_height=600, default_width=800)
-    return chart_html
+    return fig
 
-# Flask route to display the page with the chart
-@app.route('/')
-def index():
-    db_path = 'path_to_your_database.db'  # Path to your SQLite database
-    chart_html = generate_sunburst_chart_from_db(db_path)
-    return render_template('index.html', chart_html=chart_html)#display(HTML(chart_html))
+# Define database path and query
+db_path = 'your_path_insurance_data.db'
+query = '''
+    SELECT p.id, p.total_coverage_amount, p.type_of_insurance, p.annual_premium, p.insurer, 
+           p.insured, p.issue_date, p.renewal_date, p.policy_number, 
+           c.category_name, c.covered, c.not_covered
+    FROM insurance_policies p
+    LEFT JOIN categories c ON p.id = c.policy_id
+    '''
+
+# Fetch data from the database
+db_data = fetch_data_from_db(db_path, query)
+
+# Generate the chart using the fetched data
+fig = generate_sunburst_chart_from_db(db_data)
+fig.show()
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
