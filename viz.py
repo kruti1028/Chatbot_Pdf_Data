9432978c1fe51471808c43da4fa798c0ffa58a4f
kruti kikani <126723087+kruti1028@users.noreply.github.com>
2023-12-22 19:02:45 -0600

    Update viz.py

--- viz.py
+++ viz.py
@@ -1,27 +1,64 @@
 import os
+import sqlite3
 import fitz  # PyMuPDF
 import openai
 import plotly.graph_objects as go
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, redirect, url_for
 from werkzeug.utils import secure_filename
-import re  # Added for regular expressions
+import re  # For regular expressions
+from typing import List, Dict, Optional
 
 app = Flask(__name__)
-UPLOAD_FOLDER = '/Users/sanketdhameliya/Documents'  # Ensure this is a valid path
+UPLOAD_FOLDER = '/Users/sanketdhameliya/Documents'
 ALLOWED_EXTENSIONS = {'pdf'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.secret_key = 'your_secret_key'
+openai.api_key = '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'
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# Insurance policy information class
 class InsurancePolicyInfo:
-    def __init__(self, total_coverage_amount=0, insurance_type="", categories=None, covered_items=None, not_covered_items=None, covered_events=None, additional_info=None):
-        self.total_coverage_amount = total_coverage_amount
-        self.insurance_type = insurance_type
-        self.categories = categories if categories else []
-        self.covered_items = covered_items if covered_items else []
-        self.not_covered_items = not_covered_items if not_covered_items else []
-        self.covered_events = covered_events if covered_events else []
-        self.additional_info = additional_info if additional_info else {}
+    def __init__(self):
+        self.total_coverage_amount: Optional[str] = None
+        self.insurance_types: List[str] = []
+        self.annual_premium: Optional[str] = None
+        self.insurer: Optional[str] = None
+        self.insured: List[str] = []
+        self.issue_date: Optional[str] = None
+        self.renewal_date: Optional[str] = None
+        self.policy_number: Optional[str] = None
+        self.categories: Dict[str, Dict[str, List[str]]] = {}
+
+# Database setup
+def setup_database():
+    conn = sqlite3.connect('insurance.db')
+    cursor = conn.cursor()
+    cursor.execute("DROP TABLE IF EXISTS insurance_policies")
+    cursor.execute("DROP TABLE IF EXISTS categories")
+    cursor.execute('''CREATE TABLE insurance_policies (
+                        id INTEGER PRIMARY KEY,
+                        total_coverage_amount TEXT,
+                        type_of_insurance TEXT,  
+                        annual_premium TEXT,
+                        insurer TEXT,
+                        insured TEXT,
+                        issue_date TEXT,
+                        renewal_date TEXT,
+                        policy_number TEXT
+                      )''')
+    cursor.execute('''CREATE TABLE categories (
+                        policy_id INTEGER,
+                        category_name TEXT,
+                        covered TEXT,
+                        not_covered TEXT,
+                        events_covered TEXT,
+                        FOREIGN KEY(policy_id) REFERENCES insurance_policies(id)
+                      )''')
+    conn.commit()
+    conn.close()
+
+
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -32,180 +69,210 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    print("Upload route hit")  # Debug print
     if 'file' not in request.files:
-        print("No file part")  # Debug print
-        return 'No file part'
+        flash('No file part')
+        return redirect(request.url)
     file = request.files['file']
-    if file.filename == '':
-        print("No selected file")  # Debug print
-        return 'No selected file'
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(filepath)
         text = extract_text_from_pdf(filepath)
         policy_info = query_openai_and_parse(text)
-
+        insert_data_into_database(policy_info)
         chart_html = generate_sunburst_chart(policy_info)
-        print("Chart HTML generated")  # Debug print
-
         return render_template('screen2.html', chart_html=chart_html)
     else:
-        print("Invalid file type")  # Debug print
-        return 'Invalid file type'
+        flash('Invalid file type')
+        return redirect(request.url)
 
 def extract_text_from_pdf(pdf_path):
-    try:
-        doc = fitz.open(pdf_path)
-        text = ""
-        for page in doc:
-            text += page.get_text()
-        doc.close()
-        return text
-    except Exception as e:
-        print(f"Error processing PDF: {e}")
-        return ""
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    doc.close()
+    return text
 
 def query_openai_and_parse(text):
-    # Define the prompt first
+    truncated_text = text[:3000]  # Truncate to fit within token limits
+
     prompt = (
-        "I need a structured summary of this insurance policy, focusing on specific details. "
-        "Please list the following: total coverage amount, type of insurance, "
-        "categories of insurance covered, what is covered under each category, "
-        "what is not covered under each category, covered events, "
-        "annual premium, name of the insurer, name of the insured, "
-        "issue date , renewal date and policy number"
+        "Please provide a structured summary of this insurance policy in a list format, focusing on specific details. "
+        "Include the following information: \n"
+        "Total coverage amount\n"
+        "Type of insurance\n"
+        "Categories of insurance covered\n"
+        "Details of what is covered under each category\n"
+        "Details of what is not covered under each category\n"
+        "Covered events\n"
+        "Annual premium\n"
+        "Name of the insurer\n"
+        "Name of the insured\n"
+        "Issue date\n"
+        "Renewal date\n"
+        "Policy number\n" + truncated_text
     )
 
-    # Then calculate the maximum length for the text
-    max_length = 4097 - len(prompt)
-
-    # Truncate the text to fit within the model's maximum context length
-    truncated_text = text[:max_length]
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": prompt + truncated_text}]
+    response = openai.Completion.create(
+        model="text-davinci-003", 
+        prompt=prompt,
+        max_tokens=1000  # Limit the completion to 1000 tokens
     )
-    print("OpenAI Response:", response)  # Debug print
-
-    # Extract information from OpenAI response
-    extracted_text = response.choices[0].message['content']
+    extracted_text = response.choices[0].text
+    print(extracted_text)
     return parse_openai_response(extracted_text)
 
-# Extract total coverage amount using regular expressions
-def extract_total_coverage_amount(text):
-    total_coverage_amount = None
 
-    if isinstance(text, str):
-        # Use regular expressions to find the total coverage amount in various formats
-        match_million = re.search(r'BENEFIT_AMOUNT:\s*([\w\s$]+)', text)
-        match_dollars = re.search(r'PREMIUM:\s*([\w\s$]+)', text)
-
-        if match_million:
-            total_coverage_amount = match_million.group(1).strip()
-        elif match_dollars:
-            total_coverage_amount = match_dollars.group(1).strip()
-
-    return total_coverage_amount
-
-def parse_openai_response(response_text):
+def parse_openai_response(extracted_text):
     policy_info = InsurancePolicyInfo()
 
-    # Split the response text into lines
-    lines = response_text.split('\n')
+    # Basic information extraction
+    policy_info.total_coverage_amount = extract_value_by_keyword(extracted_text, "Total Coverage Amount")
+    policy_info.annual_premium = extract_value_by_keyword(extracted_text, "Annual Premium")
+    policy_info.insurer = extract_value_by_keyword(extracted_text, "Name of the Insurer")
+    insured_str = extract_value_by_keyword(extracted_text, "Name of the Insured")
+    policy_info.insured = [name.strip() for name in re.split(r' & | and ', insured_str)] if insured_str else []
+    policy_info.issue_date = extract_value_by_keyword(extracted_text, "Issue Date")
+    policy_info.renewal_date = extract_value_by_keyword(extracted_text, "Renewal Date")
+    policy_info.policy_number = extract_value_by_keyword(extracted_text, "Policy Number")
 
-    for line in lines:
-        # Extract total coverage amount
-        match_coverage_amount = re.search(r'Total Coverage Amount: (.*?)$', line)
-        if match_coverage_amount:
-            policy_info.total_coverage_amount = match_coverage_amount.group(1).strip()
+    # Extracting types of insurance and categories
+    insurance_types_str = extract_value_by_keyword(extracted_text, "Type of Insurance")
+    policy_info.insurance_types = [insurance_type.strip() for insurance_type in insurance_types_str.split(',')] if insurance_types_str else []
 
-        # Extract type of insurance
-        match_insurance_type = re.search(r'Type of Insurance: (.*?)$', line)
-        if match_insurance_type:
-            policy_info.insurance_type = match_insurance_type.group(1).strip()
+    policy_info.categories = parse_categories(extracted_text)
 
-        # Extract categories of insurance covered
-        if line.startswith(str(len(policy_info.categories) + 1) + '. '):
-            categories_text = line.split(': ')[1].strip()
-            policy_info.categories.append(categories_text)
-
-        # Extract what is covered under each category
-        if line.startswith(str(len(policy_info.covered_items) + 1) + '. '):
-            covered_items_text = line.split(': ')[1].strip()
-            policy_info.covered_items.append(covered_items_text)
-
-        # Extract what is not covered under each category
-        if line.startswith(str(len(policy_info.not_covered_items) + 1) + '. '):
-            non_covered_items_text = line.split(': ')[1].strip()
-            policy_info.not_covered_items.append(non_covered_items_text)
-
-        # Extract covered events
-        if line.startswith('- '):
-            covered_events_text = line.strip('- ').strip()
-            policy_info.covered_events.append(covered_events_text)
-
-        # Extract additional information based on keywords and patterns
-        match_additional_info = re.search(r'(Annual Premium|Insurer|Insured|Issue Date|Renewal Date|Policy Number): (.*?)$', line)
-        if match_additional_info:
-            info_type = match_additional_info.group(1)
-            info_value = match_additional_info.group(2)
-            policy_info.additional_info[info_type] = info_value.strip()
-
+    print("Parsed Data:", policy_info.__dict__)
     return policy_info
 
+def parse_categories(text):
+    categories = {}
+    
+    # Assuming categories are listed under "Categories of Insurance Covered"
+    categories_str = extract_value_by_keyword(text, "Categories of Insurance Covered")
+    if categories_str:
+        category_names = [cat.strip() for cat in categories_str.split(',')]
+        for category in category_names:
+            categories[category] = {"covered": [], "not_covered": [], "events_covered": []}
+
+            # Extract details for each category
+            covered_str = extract_value_by_keyword(text, f"Details of What is Covered under {category}")
+            if covered_str:
+                categories[category]["covered"] = [item.strip() for item in covered_str.split(',')]
+
+            not_covered_str = extract_value_by_keyword(text, f"Details of What is Not Covered under {category}")
+            if not_covered_str:
+                categories[category]["not_covered"] = [item.strip() for item in not_covered_str.split(',')]
+
+    # Additional parsing logic for other details as required
+
+    return categories
+
+def extract_value_by_keyword(text, keyword):
+    pattern = rf"{keyword}: ([^\n]+)"
+    match = re.search(pattern, text)
+    if match:
+        return match.group(1).strip()
+    return None
+
+def insert_data_into_database(policy_info):
+    conn = sqlite3.connect('insurance.db')
+    cursor = conn.cursor()
+
+    # Convert the 'insured' list to a string
+    insured_str = ', '.join(policy_info.insured) if policy_info.insured else ''
+    type_of_insurance_str = ', '.join(policy_info.insurance_types) if policy_info.insurance_types else ''
+
+    # Insert main policy data
+    cursor.execute('''INSERT INTO insurance_policies 
+                      (total_coverage_amount, type_of_insurance, annual_premium, insurer, insured, issue_date, renewal_date, policy_number)
+                      VALUES (?, ?, ?, ?, ?, ?, ?, ?)''', 
+                   (policy_info.total_coverage_amount, type_of_insurance_str, policy_info.annual_premium, 
+                    policy_info.insurer, insured_str, policy_info.issue_date, policy_info.renewal_date, policy_info.policy_number))
+    
+    # Get the last inserted row id (the policy_id)
+    policy_id = cursor.lastrowid
+
+    # Insert category data
+    for category, details in policy_info.categories.items():
+        covered_str = ', '.join(details['covered']) if details['covered'] else ''
+        not_covered_str = ', '.join(details['not_covered']) if details['not_covered'] else ''
+        cursor.execute('''INSERT INTO categories 
+                          (policy_id, category_name, covered, not_covered)
+                          VALUES (?, ?, ?, ?)''', 
+                       (policy_id, category, covered_str, not_covered_str))
+    cursor.execute("SELECT * FROM insurance_policies")
+    print("Insurance Policies:", cursor.fetchall())
+    cursor.execute("SELECT * FROM categories")
+    print("Categories:", cursor.fetchall())
+    conn.commit()
+    conn.close()
 
 def generate_sunburst_chart(policy_info):
-    labels = ["Total Coverage"]
-    parents = [""]
-    values = [policy_info.total_coverage_amount if policy_info.total_coverage_amount else 0.0]
-    hovertext = [f"Total Coverage: {policy_info.total_coverage_amount}" if policy_info.total_coverage_amount else "Total Coverage: None"]
+    # Initialize the sunburst chart elements
+    total_coverage_amount = policy_info.total_coverage_amount or "Not specified"
+    root_label = f"Total Coverage: {total_coverage_amount}"
+    labels = [root_label]  # root node with total coverage amount
+    parents = [""]  # root has no parent
+    values = [100]  # Assign a static value or calculate based on data
+    # Detailed hover information for the center
+    hovertext = [f"Insurer: {policy_info.insurer or 'Not specified'}<br>"
+                 f"Insured: {', '.join(policy_info.insured) or 'Not specified'}<br>"
+                 f"Total Coverage Amount: {total_coverage_amount}<br>"
+                 f"Annual Premium: {policy_info.annual_premium or 'Not specified'}<br>"
+                 f"Renewal Date: {policy_info.renewal_date or 'Not specified'}<br>"
+                 f"Issue Date: {policy_info.issue_date or 'Not specified'}"]
 
-    if policy_info.insurance_type:
-        labels.append(policy_info.insurance_type)
-        parents.append("Total Coverage")
-        values.append(policy_info.total_coverage_amount / 2 if policy_info.total_coverage_amount else 0.0)
-        hovertext.append(f"Insurance Type: {policy_info.insurance_type}")
+    # Add insurance types as the first layer
+    for insurance_type in policy_info.insurance_types:
+        labels.append(insurance_type)
+        parents.append(root_label)
+        values.append(20)  # Adjust value as necessary
+        hovertext.append(f"Type of Insurance: {insurance_type}")
 
-    for category in policy_info.categories:
-        labels.append(category)
-        parents.append(policy_info.insurance_type if policy_info.insurance_type else "Total Coverage")
-        values.append(1.0)
-        hovertext.append(f"Category: {category}")
+        # Add categories for each insurance type as the second layer
+        for category, details in policy_info.categories.get(insurance_type, {}).items():
+            labels.append(category)
+            parents.append(insurance_type)
+            values.append(10)  # Adjust value as necessary
+            hovertext.append(f"Category: {category}")
 
-    for item in policy_info.covered_items:
-        labels.append(item)
-        parents.append(policy_info.categories[0] if policy_info.categories else (policy_info.insurance_type if policy_info.insurance_type else "Total Coverage"))
-        values.append(1.0)
-        hovertext.append(f"Covered Item: {item}")
+            # Add covered and not covered items as the third and fourth layers
+            # Handling covered items
+            for item in details.get('covered', []):
+                labels.append(f"Covered: {item}")
+                parents.append(category)
+                values.append(5)  # Adjust value as necessary
+                hovertext.append(f"Covered Item: {item}")
 
-    for item in policy_info.not_covered_items:
-        labels.append(item)
-        parents.append(policy_info.categories[0] if policy_info.categories else (policy_info.insurance_type if policy_info.insurance_type else "Total Coverage"))
-        values.append(1.0)
-        hovertext.append(f"Not Covered Item: {item}")
+            # Handling not covered items
+            for item in details.get('not_covered', []):
+                labels.append(f"Not Covered: {item}")
+                parents.append(category)
+                values.append(5)  # Adjust value as necessary
+                hovertext.append(f"Not Covered Item: {item}")
 
-    for event in policy_info.covered_events:
-        labels.append(event)
-        parents.append(policy_info.categories[0] if policy_info.categories else (policy_info.insurance_type if policy_info.insurance_type else "Total Coverage"))
-        values.append(1.0)
-        hovertext.append(f"Covered Event: {event}")
-
+    # Create and configure the sunburst chart
     fig = go.Figure(go.Sunburst(
         labels=labels,
         parents=parents,
         values=values,
+        hoverinfo="text",
         hovertext=hovertext,
-        hoverinfo="text"
+        branchvalues="total"
     ))
-
     fig.update_layout(margin=dict(t=0, l=0, r=0, b=0))
+    print("Labels:", labels)
+    print("Parents:", parents)
+    print("Values:", values)
+    print("Hovertext:", hovertext)
 
-    chart_html = fig.to_html(full_html=False, include_plotlyjs=False, default_height=400, default_width=600)
-
+    # Convert chart to HTML
+    chart_html = fig.to_html(full_html=False, include_plotlyjs='cdn', default_height=600, default_width=800)
     return chart_html
 
 if __name__ == '__main__':
+    setup_database()
     app.run(debug=True, port=8080)
+
